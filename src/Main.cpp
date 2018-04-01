63751c52f69bdb79506cc2cf6e8412f45b19ed1e
kk <krishnakarthik1309@gmail.com>
2018-04-01 11:13:08 +0530

    add changeSize method

--- src/Main.cpp
+++ src/Main.cpp
@@ -67,9 +67,49 @@ void processKeys(unsigned char key, int xx, int yy)
 
 }
 
+void setIdentityMatrix( float *mat, int size) {
+
+	// fill matrix with 0s
+	for (int i = 0; i < size * size; ++i)
+		mat[i] = 0.0f;
+
+	// fill diagonal with 1s
+	for (int i = 0; i < size; ++i)
+		mat[i + i * size] = 1.0f;
+}
+
+void buildProjectionMatrix(float fov, float ratio, float nearp, float farp)
+{
+	float projMatrix[16];
+	float f = 1.0f / tan (fov * (M_PI / 360.0f));
+
+	setIdentityMatrix(projMatrix,4);
+
+	projMatrix[0] = f / ratio;
+	projMatrix[1 * 4 + 1] = f;
+	projMatrix[2 * 4 + 2] = (farp + nearp) / (nearp - farp);
+	projMatrix[3 * 4 + 2] = (2.0f * farp * nearp) / (nearp - farp);
+	projMatrix[2 * 4 + 3] = -1.0f;
+	projMatrix[3 * 4 + 3] = 0.0f;
+
+	glBindBuffer(GL_UNIFORM_BUFFER, matricesUniBuffer);
+	glBufferSubData(GL_UNIFORM_BUFFER, ProjMatrixOffset, MatrixSize, projMatrix);
+	glBindBuffer(GL_UNIFORM_BUFFER, 0);
+}
+
 void changeSize(int w, int h)
 {
+	float ratio;
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window of zero width).
+	if(h == 0)
+		h = 1;
 
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
+
+	ratio = (1.0f * w) / h;
+	buildProjectionMatrix(53.13f, ratio, 0.1f, 100.0f);
 }
 
 void renderScene(void)
@@ -133,6 +173,19 @@ int main(int argc, char **argv)
 	glutMouseFunc(processMouseButtons);
 	glutMotionFunc(processMouseMotion);
 	glutMouseWheelFunc ( mouseWheel ) ;
+
+	//	Init GLEW
+	glewInit();
+	if (glewIsSupported("GL_VERSION_3_3"))
+		printf("Ready for OpenGL 3.3\n");
+	else {
+		printf("OpenGL 3.3 not supported\n");
+		return(1);
+	}
+
+	//  Init the app (load model and textures) and OpenGL
+	if (!init())
+		printf("Could not Load the Model\n");
 
 	// version info
 	printf ("Vendor: %s\n", glGetString (GL_VENDOR));
--- src/shaders.h
+++ src/shaders.h
@@ -1,3 +1,4 @@
 extern GLuint vertexLoc, normalLoc, texCoordLoc, matricesUniLoc, materialUniLoc, program, vertexShader, fragmentShader;
+extern GLuint texUnit;
 
 GLuint setupShaders(char *vertexFileName, char *fragmentFileName);
